d96c2cef0b55aa0a496c07c0f7b0c42ba529470e
jessica.jiang <jessica.jiang@royole.com>
2019-01-11 17:31:00 +0800

    添加旋转效果

--- app/src/main/java/com/jsj/openglpractice1/MyGLSurfaceView.java
+++ app/src/main/java/com/jsj/openglpractice1/MyGLSurfaceView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -16,6 +17,8 @@ class MyGLSurfaceView extends GLSurfaceView {
     private final float[] mMVPMatrix = new float[16];
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
+
+    private float[] mRotationMatrix = new float[16];
 
 
 
@@ -57,6 +60,8 @@ class MyGLSurfaceView extends GLSurfaceView {
 
         @Override
         public void onDrawFrame(GL10 gl) {
+            float[] scratch = new float[16];
+
             // Redraw background color
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
@@ -66,10 +71,22 @@ class MyGLSurfaceView extends GLSurfaceView {
             //Calculate the projection and view transformation
             Matrix.multiplyMM(mMVPMatrix,0,mProjectionMatrix,0,mViewMatrix,0);
 
+            //Create a rotation transformation for the trangle
+            long time = SystemClock.uptimeMillis() % 4000L;
+            float angle = 0.090f * ((int) time);
+            Matrix.setRotateM(mRotationMatrix,0,angle,0,0,-1.0f);
+
+            //Combine the rotation matrix with the projection and camera view
+            //Note that the mMVPMatrix factor *must be first in order
+            //for the matrix multiplication product to be correct.
+            Matrix.multiplyMM(scratch,0,mMVPMatrix,0,mRotationMatrix,0);
+
             //Draw shape1
-            //mTriangle.draw();
+            //  mTriangle.draw();
             //Draw shape2
-            mTriangle.draw(mMVPMatrix);
+            // mTriangle.draw(mMVPMatrix);
+            //Draw triangle3
+            mTriangle.draw(scratch);
 
         }
     }
